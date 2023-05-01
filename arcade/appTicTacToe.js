da83db7999a9fd27684a01c7551bd46a85c0cbb6
Jack-Tillman <johntillman345@gmail.com>
2023-05-01 17:28:45 -0400

    minor revisions, mostly formatting

--- arcade/appTicTacToe.js
+++ arcade/appTicTacToe.js
@@ -1,5 +1,28 @@
 //THIS IS FOR TICTACTOE
 /*
+User Interface components
+- Title of game 
+- 3 x 3 grid 
+    * each cell is clickable 
+    * each cell has an indication of which player filled it in (when applicable) 
+- Message indicating which player's turn it is 
+    * this message box will also display who wins the game 
+    * or if no winners, then it will display a tie 
+- Restart button
+    * This restarts the game by setting the game state back to initial 
+
+Game flow components
+- Must keep track of clicks that happen on each cell
+    * player is blocked from trying to fill in an already filled cell and nothing happens
+- Game state is updated 
+- Game state is validated 
+    * Check if there is a winner or if there is a draw 
+- If above results in neither winner or tie, then change the active player
+    * else, stop the game 
+- Update the UI to reflect changes to game state
+- Repeat until a winner or a tie is reached
+
+
 What is the starting state?
 - The starting state is an empty board
 How do I display the state of the game to the user?
@@ -18,36 +41,52 @@ How does each interaction update the state?
     - The 
 - Each 
 */
-"use strict";
 
-//Game State
-let state;
 
-//function to build the initial game state
-function buildInitialState(){
-    const gameState = {
-        players: ['x', 'o'],
-        board: [
-            [null, null, null],
-            [null, null, null],
-            [null, null, null]
-        ]
-    };
 
- return gameState;   
-}
 
-//renders the game state
-function renderState() {
 
-}
 
-//listeners
-function onBoardClick() {
-    // update state, maybe with another dozen or so helper functions...
+
+
+
+
+
+
+
+
+
+
+
+
+// //Game State
+// let state;
+
+// //function to build the initial game state
+// function buildInitialState(){
+//     const gameState = {
+//         players: ['x', 'o'],
+//         board: [
+//             [null, null, null],
+//             [null, null, null],
+//             [null, null, null]
+//         ]
+//     };
+
+//  return gameState;   
+// }
+
+// //renders the game state
+// function renderState() {
+
+// }
+
+// //listeners
+// function onBoardClick() {
+//     // update state, maybe with another dozen or so helper functions...
   
-    renderState() // show the user the new state
-  }
+//     renderState() // show the user the new state
+//   }
 
-const gameBoard = document.getElementById('board');
-gameBoard.addEventListener('click', onBoardClick);
+// const gameBoard = document.getElementById('board');
+// gameBoard.addEventListener('click', onBoardClick);
