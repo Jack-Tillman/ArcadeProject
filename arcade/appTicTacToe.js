4668f64c3dcc32edffdfeacae222a80d7ea50953
Jack-Tillman <johntillman345@gmail.com>
2023-05-02 17:37:20 -0400

    Added randomMark(), getPlayerOneName(),getPlayerTwoName(), and minor revisions. Honestly, I think I should redo a lot of this game - not utilizing objects from the start was a fool's decision, and I am facing the repercussions.

--- arcade/appTicTacToe.js
+++ arcade/appTicTacToe.js
@@ -1,3 +1,4 @@
+'use strict';
 //THIS IS FOR TICTACTOE
 /*
 User Interface components
@@ -64,17 +65,51 @@ const winningMessageElement = document.getElementById('winningMessage');
 const winningMessageTextElement = document.querySelector('[data-winning-message-text]')
 //variable for restartButton
 const restartButton = document.getElementById('restartButton');
+//variable for the span to display player names 
+const playerOneName = document.getElementById('playerOneName');
+const playerTwoName = document.getElementById('playerTwoName');
+
+function getPlayerOneName() {
+    let playerOne = null;
+    while (playerOne === null || !isNaN(playerOne)) {
+        playerOne = prompt("What is the name of Player 1? ");
+    
+        if(playerOne === null || !isNaN(playerOne)) {
+            alert("Invalid name, try entering another name!");
+        } else {
+          return playerOneName.innerText = `Player 1 is: ${playerOne}!`;
+        }
+    }
+};
+
+function getPlayerTwoName() {
+    let playerTwo = null;
+    while (playerTwo === null || !isNaN(playerTwo)) {
+        playerTwo = prompt("What is the name of Player 2? If you wish to play against a computer, simply type the word, computer, in the field and press OK! ");
+        
+        if(playerTwo === null || !isNaN(playerTwo)) {
+            alert("Invalid name, try entering another name!");
+        } else if (playerTwo == "computer" || playerTwo == "Computer"){
+          alert("You will play against a computer opponent, then!");
+          return playerTwoName.innerText = `Player 2 is: ${playerTwo}!`;
+          } else {
+          return playerTwoName.innerText = `Player 2 is: ${playerTwo}!`;
+        }
+    }
+};
+
 
 startGame();
 
-restartButton.addEventListener('click', startGame);
 
+restartButton.addEventListener('click', startGame);
 
 //loop through data-cell nodelist 
 cellElements.forEach(cell => {
     //add event listener to each cell. when clicked, it calls handleClick() ; once:true means that handleClick can only be called once per click event per cell
     cell.addEventListener('click', handleClick, {once: true})
 })
+
 
 function handleClick(e){
     //whichever cell we clicked on will be the target
@@ -91,13 +126,32 @@ function handleClick(e){
         //only switch turns and enable hover effects if the game is not over
         switchTurn();
         setBoardHoverClass();
+        placeMark(randomMark(),currentClass);
+        
     }
+}
+
+//randomize whether X or O goes first 
+function turnOrder() {
+let turnOrder = Math.random();
+//if true, circle has first move. if false, X has first move. 
+return turnOrder > 0.50;
 }
 
 //function to start the game
 function startGame() {
-    //arbitrary assignment *LIKELY CHANGE LATER
-    circleTurn = false;
+    getPlayerOneName();
+    getPlayerTwoName();
+    //depending on random output from turnOrder, either circle or X will go first
+    circleTurn = turnOrder();
+    //playerOne goes first, so if circleTurn is true, playerOne plays as Circle
+    if (circleTurn) {
+        playerOneName.innerText += `  You will play as O!`;
+        playerTwoName.innerText += `  You will play as X!`;
+    } else {
+        playerOneName.innerText += `  You will play as X!`;
+        playerTwoName.innerText += `  You will play as O!`;
+    }
     //loop through data-cell nodelist and add event listener to each cell, same as above
     cellElements.forEach(cell => {
         cell.classList.remove(X_CLASS);
@@ -105,17 +159,49 @@ function startGame() {
         cell.removeEventListener('click', handleClick);
         cell.addEventListener('click', handleClick, {once: true})
     })
-    //call setBoardHoverClass to enable functionality at beginning of game 
+    //call setBoardHoverClass to enable hover functionality at beginning of game 
     setBoardHoverClass();
-    //after game is over, start removing the non-initial classes and such added during the game
+    //after game is over, remove the non-initial classes during the game
     winningMessageElement.classList.remove('show');
-
 }
 
 //add a class name that matches the currentClass to the cell that is clicked 
 function placeMark(cell, currentClass) {
     cell.classList.add(currentClass);
 }
+//incomplete and road blocked - might need to just redo a  lot of this project honestly! 
+//initialize an empty array, add cells that have no X or CIRCLE class to it, then select a random cell from that array and return it. that returned cell would then be passed through placeMark to simulate a computer selecting a random cell
+function randomMark() {
+    let potentialCells = [];
+    for (let i = 0; i < cellElements.length; i++){
+        let currentCell = cellElements[i];
+        if (currentCell.classList.contains(X_CLASS) || currentCell.classList.contains(CIRCLE_CLASS)) {
+                continue;
+            }else {
+                 potentialCells.unshift(cell);
+                    for (cell of potentialCells) {
+                        cell = currentCell.click();
+                        return cell;
+                    }
+            }
+        }
+    }
+
+
+// function randomMark() {
+//     for (let i = 0; i < cellElements.length; i++){
+//         let currentCell = cellElements[i];
+//         for (let j = 0; j < 1; j++){
+//             if (currentCell.classList.contains(X_CLASS) || currentCell.classList.contains(CIRCLE_CLASS)) {
+//                 continue;
+//             }else {
+//                 cell = currentCell.click();
+//                 return cell;
+//             }
+//         }
+//     }
+// }
+
 //every turn, reassign circleTurn the inverse value to switch player turn
 function switchTurn() {
     circleTurn = !circleTurn;
@@ -123,15 +209,15 @@ function switchTurn() {
 //re-add the hover effects 
 function setBoardHoverClass() {
 //remove both X and CIRCLE classes from board constant 
-board.classList.remove(X_CLASS);
-board.classList.remove(CIRCLE_CLASS);
+    board.classList.remove(X_CLASS);
+    board.classList.remove(CIRCLE_CLASS);
 //if it is the circle player's turn, add CIRCLE_CLASS to board
-if (circleTurn) {
-    board.classList.add(CIRCLE_CLASS);
+    if (circleTurn) {
+        board.classList.add(CIRCLE_CLASS);
 //if it is the X player's turn, add X_CLASS to board
-} else {
-    board.classList.add(X_CLASS);
-}
+    } else {
+        board.classList.add(X_CLASS);
+    }
 }
 //check all winning boards and see if they are met by the current board
 //Go 
@@ -167,6 +253,35 @@ function isDraw() {
     })
 }
 
+/*
+to do:
+- make the results screen say playerName wins
+- pretty up fonts and layout in general, colors
+
+IDEAS:
+each turn, computer checks move availability
+randomly chooses one of the available spaces 
+maybe use append, class change, etc to do that
+
+Computer looks through available cells for the attribute of whether it has been clicked or not
+
+
+enable singleplayer vs multiplayer
+
+
+- Upon visiting the sight, user is prompted to enter the name of Player 1 and Player 2 
+    - For the player 2 popup, state that if there is no second player, then just press enter (if playerName = '', use computerplay function )
+    - 
+    - if multiplayer is selected,
+        - User is prompted to enter the name of player1 / Xs and also the name of player2 / Os
+        - Turn is decided by random function 
+Enable computer play 
+    - Can utilize classList and Math.random() 
+        - Computer should check the class of each cell for empty spaces, then choose a space at random with Math.random to place the mark
+Enable players to input name 
+-prompt function, store the strings in a variable, display that variable 
+Display player and computer name    
+*/
 
 
 
